e563eb2c76f5fc341d201a681bfd9adb1eccd444
zianfanti <coconeltero@gmail.com>
2025-12-08 11:27:50 -0600

    add test on real curves

--- predict_curve.py
+++ predict_curve.py
@@ -1,7 +1,17 @@
 import os
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+# os.environ["OMP_NUM_THREADS"] = "1"
+# os.environ["MKL_NUM_THREADS"] = "1"
+
+import cv2
 import numpy as np
 import torch
+import yaml
 
+import utils.Smoothing as smooth
+
+from matplotlib import pyplot as plt
+from scc_tree.SCC_Tree import SCC_Tree
 from gru_model_evaluation import (
     CurveToWModel,
     CurveDataset,
@@ -34,6 +44,38 @@ def predict_w_from_curve(model, new_curve, device="cpu"):
 
 
 
+def open_tree_def(config_file_path):
+    with open(config_file_path, 'r') as file:
+        config_data = yaml.safe_load(file)
+
+    sp = (config_data["start_position"]["y"], config_data["start_position"]["x"])
+    image_path = config_data["base_folder"] + config_data["binary_image"]
+
+    assert os.path.isfile(image_path), "The image {} doesn't exixt".format(config_data["binary_image"])
+    o_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    return [o_image, sp]
+
+
+
+def plot_results(curve1, curve2):
+    """
+    Plot the results of the polynomial fitting
+    """
+    plt.figure(figsize=(12, 12))
+    plt.plot(curve1[:, 0], curve1[:, 1], 'bo-', alpha=0.3, markersize=2, linewidth=1, label='Original')
+    plt.plot(curve2[:, 0], curve2[:, 1], 'ro-', alpha=0.8, markersize=2, linewidth=1, label='Smoothed')
+
+
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.title('Curve Smoothing with Cubic Spline')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.axis('equal')
+    plt.show()
+
+
+
 def main():
     # Must match what you used in training
     config = TrainingConfig(
@@ -57,11 +99,24 @@ def main():
 
     # Load or construct a new curve: shape (n, 2)
     # Example dummy curve:
-    n = 50
-    new_curve = np.random.randn(n, 2).astype(np.float32)
+    # n = 50
+    # new_curve = np.random.randn(n, 2).astype(np.float32)
 
-    w_pred = predict_w_from_curve(model, new_curve, device=device)
-    print("Predicted w:", w_pred)
+    # w_pred = predict_w_from_curve(model, new_curve, device=device)
+    # print("Predicted w:", w_pred)
+
+    [image, tree_root] = open_tree_def("/Users/zianfanti/Trabajo/tree_representation/back-forth/config.yaml")
+    scc_tree = SCC_Tree.create_from_image(image, tree_root)
+    branches = scc_tree.get_pixelated_branches()
+  
+    for branch in branches:
+        w_pred = predict_w_from_curve(model, branch, device=device)
+        points_p = abs(w_pred[0])
+        smooth_p = abs(w_pred[1])
+        smooth_curve = smooth.smooth_with_regularization(branch, arclen_points=points_p, smoothing_factor=smooth_p)
+        plot_results(branch, smooth_curve)
+        print("Predicted w:", w_pred)
+        
  
 
 if __name__ == "__main__":
